ca5ef545db51e011f94cbc4511ac8eb5308cdfa8
Meg Chantel Hinton <89792778+MegApril@users.noreply.github.com>
2025-05-05 16:44:17 -0600

    Update 03_standardize.sql

--- sql/03_standardize.sql
+++ sql/03_standardize.sql
@@ -1,8 +1,9 @@
-
+-- made all casing the same for consistency.
 SELECT
   UPPER(TRIM(Maternal_Age)) AS cleaned_maternal_age,
   COUNT(*) AS count
 FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step1_deduped`
 GROUP BY cleaned_maternal_age
 ORDER BY count DESC;
+
 
