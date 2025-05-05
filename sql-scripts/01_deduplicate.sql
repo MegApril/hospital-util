ccfd765bda934d5045e75e6ee183ad7912ac5735
Meg Chantel Hinton <89792778+MegApril@users.noreply.github.com>
2025-05-05 01:41:04 -0600

    Update 01_deduplicate.sql
    Added script

--- sql-scripts/01_deduplicate.sql
+++ sql-scripts/01_deduplicate.sql
@@ -1,1 +1,7 @@
+-- STEP 0: Start from raw data (do NOT modify this)
+-- `maternal-neonatal-outcomes.neonatal_maternal_data.interventions_q1` is your original dataset
 
+-- STEP 1: Remove Exact Duplicates
+CREATE OR REPLACE TABLE `maternal-neonatal-outcomes.neonatal_maternal_data.step1_deduped` AS
+SELECT DISTINCT *
+FROM `maternal-neonatal-outcomes.neonatal_maternal_data.interventions_staging` 
