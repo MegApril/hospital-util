bd42b22373e88534e79eb73ae960103624d87512
Meg Chantel Hinton <89792778+MegApril@users.noreply.github.com>
2025-05-05 16:40:21 -0600

    Update 02_trim_and_case.sql
    moving standardized code to 03_standardize vs 02_trim_and_case

--- sql/02_trim_and_case.sql
+++ sql/02_trim_and_case.sql
@@ -1,7 +1,1 @@
-SELECT
-  UPPER(TRIM(Maternal_Age)) AS cleaned_maternal_age,
-  COUNT(*) AS count
-FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step1_deduped`
-GROUP BY cleaned_maternal_age
-ORDER BY count DESC;
 
