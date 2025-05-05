1d07d94880d13d55840b2708c6e8bca13f65c1e2
Meg Chantel Hinton <89792778+MegApril@users.noreply.github.com>
2025-05-05 16:37:48 -0600

    Update 02_trim_and_case.sql
    Maternal_Age cleaning

--- sql/02_trim_and_case.sql
+++ sql/02_trim_and_case.sql
@@ -1,1 +1,7 @@
+SELECT
+  UPPER(TRIM(Maternal_Age)) AS cleaned_maternal_age,
+  COUNT(*) AS count
+FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step1_deduped`
+GROUP BY cleaned_maternal_age
+ORDER BY count DESC;
 
