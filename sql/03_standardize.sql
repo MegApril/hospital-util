46f2621361a67ef1021b5f90675856c5e3e5c44d
Meg Chantel Hinton <89792778+MegApril@users.noreply.github.com>
2025-05-07 12:40:57 -0600

    Update 03_standardize.sql

--- sql/03_standardize.sql
+++ sql/03_standardize.sql
@@ -1,9 +1,28 @@
--- made all casing the same for consistency.
+-- ensure casing is the same for consistency and use a 'GROUP BY' statement to discover different attributes in fields ensuring all inconsistent misspellings are accounted for
 SELECT
-  UPPER(TRIM(Maternal_Age)) AS cleaned_maternal_age,
+  cleaned_risk_factors
   COUNT(*) AS count
-FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step1_deduped`
-GROUP BY cleaned_maternal_age
+FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step2_trimmed`
+GROUP BY cleaned_risk_factors
 ORDER BY count DESC;
 
+SELECT
+  cleaned_insurance,
+  COUNT(*) AS count
+FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step2_trimmed`
+GROUP BY cleaned_insurance
+ORDER BY count DESC;
 
+-- use case statements to standardize misspellings and save into a new table
+CREATE OR REPLACE TABLE `maternal-neonatal-outcomes.neonatal_maternal_data.step3_standardized` AS
+SELECT
+  CASE
+    WHEN cleaned_risk_factors IN ('hypertensoin') THEN 'hypertension'
+    ELSE cleaned_risk_factors
+  END AS standardized_risk_factors
+
+  CASE
+    WHEN cleaned_insurance IN ('medcaid') THEN 'medicaid'
+    ELSE cleaned_insurance
+  END AS standardized_insurance
+FROM `maternal-neonatal-outcomes.neonatal_maternal_data.step2_trimmed`;
